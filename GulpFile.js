b6aeb4f76192faee0b408e933b41c34c6a6e7bf3
dudusbarros <joseeduardobarros@gmail.com>
2016-10-14 10:47:06 -0300

    Update dist images Update index

--- GulpFile.js
+++ GulpFile.js
@@ -149,7 +149,7 @@ gulp.task('js-watch', ['js'], function () {
 gulp.task('resizePhotos', function () {
 	gulp.src(source.images.photos.location + source.images.photos.content)
 		.pipe(imageResize({
-			height : 960,
+			width : 1080,
 			upscale : false
 		}))
 		.pipe(gulp.dest(dist.images.location));
